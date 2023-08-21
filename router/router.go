6b8694c4e05f18b258e3eacd3ed559ec2db51be6
big cock <you@example.com>
2023-08-20 22:33:13 -0400

    go is good :)

--- router/router.go
+++ router/router.go
@@ -2,7 +2,7 @@ package router
 
 import (
 	"github.com/gofiber/fiber/v2"
-	
+
 	api "tritan.dev/image-uploader/handlers/api"
 	ui "tritan.dev/image-uploader/handlers/ui"
 )
@@ -10,9 +10,11 @@ import (
 func SetupRoutes(app *fiber.App) error {
 	app.Get("/", ui.LoadUploaderPage)
 	app.Get("/:dir/:file", ui.DisplayImage)
+
 	app.Post("/api/upload", api.Upload)
 	app.Post("/api/create-key", api.CreateKey)
 	app.Post("/api/sharex-config", api.GetShareXConfig)
+
 	app.Static("/api/content/raw", "./uploads")
 
 	return nil
