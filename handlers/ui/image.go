6e0715104717998d22e6b8112141ed8f7e75a90f
xfy <135300152+xfy777@users.noreply.github.com>
2023-09-02 04:10:50 -0400

    same thing again

--- handlers/ui/image.go
+++ handlers/ui/image.go
@@ -11,49 +11,51 @@ import (
 )
 
 func DisplayImage(c *fiber.Ctx) error {
-	dir := c.Params("dir")
-	file := c.Params("file")
+    dir := c.Params("dir")
+    file := c.Params("file")
 
-	uploadsDir := functions.GetPath("./uploads")
+    uploadsDir := functions.GetPath("./uploads")
 
-	fileWithoutExtension, err := functions.FindFileWithoutExtension(file, uploadsDir)
-	if err != nil {
-		fmt.Println(err)
-		return c.Status(500).JSON(fiber.Map{
-			"status":  500,
-			"message": "Internal server error",
-		})
-	}
+    resultCh := make(chan fiber.Map, 1)
+    errorCh := make(chan error, 1)
 
-	if !isValidDir(dir) || fileWithoutExtension == "" {
-		return c.Status(404).JSON(fiber.Map{
-			"status":  404,
-			"message": "Content not found.",
-		})
-	}
+    go func() {
+        fileWithoutExtension, err := functions.FindFileWithoutExtension(file, uploadsDir)
+        if err != nil {
+            errorCh <- err
+            return
+        }
 
-	if fileWithoutExtension != "" {
-		data := map[string]interface{}{
-			"Data": map[string]string{
-				"Name": fileWithoutExtension,
-				"Ext":  path.Ext(fileWithoutExtension),
-			},
-		}
+        if !isValidDir(dir) || fileWithoutExtension == "" {
+            errorCh <- fmt.Errorf("Content not found") 
+            return
+        }
 
-		return c.Render("./pages/image.html", data)
-	}
+        if fileWithoutExtension != "" {
+            data := map[string]interface{}{
+                "Data": map[string]string{
+                    "Name": fileWithoutExtension,
+                    "Ext":  path.Ext(fileWithoutExtension),
+                },
+            }
+            resultCh <- data 
+        }
+    }()
 
-	return c.Status(404).JSON(fiber.Map{
-		"status":  404,
-		"message": "Content not found.",
-	})
-}
-
-func isValidDir(dir string) bool {
-	for _, validDir := range config.AppConfigInstance.Dirs {
-		if strings.EqualFold(dir, validDir) {
-			return true
-		}
-	}
-	return false
+    select {
+    case data := <-resultCh:
+        if data == nil {
+            return c.Status(404).JSON(fiber.Map{
+                "status":  404,
+                "message": "Content not found.",
+            })
+        }
+        return c.Render("./pages/image.html", data)
+    case err := <-errorCh:
+        fmt.Println(err) 
+        return c.Status(500).JSON(fiber.Map{
+            "status":  500,
+            "message": "Internal server error",
+        })
+    }
 }
