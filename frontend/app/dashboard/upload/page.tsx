f932b17f3e9a983fa5e336db0d86d5b5bc3d2c1e
big dick bandit <dylan@tritan.gg>
2025-02-17 02:32:50 -0500

    Edited dropzone styling and such

--- frontend/app/dashboard/upload/page.tsx
+++ frontend/app/dashboard/upload/page.tsx
@@ -5,8 +5,9 @@ import { useTokenStore } from "../../../stores/session.store";
 import { Sidebar } from "../../../components/sidebar";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Upload, FileUp } from "lucide-react";
 
-const Upload: React.FC = () => {
+const UploadPage: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = React.useState(true);
   const userStore = useTokenStore();
   const dropzoneRef = React.useRef<HTMLDivElement>(null);
@@ -24,27 +25,32 @@ const Upload: React.FC = () => {
     );
   }
 
-  const handleDragOver = (e: React.DragEvent) => {
-    e.preventDefault();
-    if (dropzoneRef.current) dropzoneRef.current.classList.add("dropzone-bg");
-  };
-
-  const handleDragLeave = (e: React.DragEvent) => {
-    e.preventDefault();
-
-    if (dropzoneRef.current)
-      dropzoneRef.current.classList.remove("dropzone-bg");
-  };
-
   const handleDrop = async (e: React.DragEvent) => {
     e.preventDefault();
-    if (dropzoneRef.current) {
-      dropzoneRef.current.classList.remove("dropzone-bg");
-    }
 
     const formData = new FormData();
     formData.append("sharex", e.dataTransfer.files[0]);
 
+    await uploadFile(formData);
+  };
+
+  const handleClick = () => {
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.style.display = "none";
+
+    fileInput.onchange = async () => {
+      const formData = new FormData();
+
+      if (fileInput.files) {
+        formData.append("sharex", fileInput.files[0]);
+        await uploadFile(formData);
+      }
+    };
+    fileInput.click();
+  };
+
+  const uploadFile = async (formData: FormData) => {
     try {
       const response = await fetch("/api/upload", {
         method: "POST",
@@ -64,40 +70,6 @@ const Upload: React.FC = () => {
     }
   };
 
-  const handleClick = () => {
-    const fileInput = document.createElement("input");
-
-    fileInput.type = "file";
-    fileInput.style.display = "none";
-
-    fileInput.onchange = async () => {
-      const formData = new FormData();
-
-      if (fileInput.files) {
-        formData.append("sharex", fileInput.files[0]);
-
-        try {
-          const response = await fetch("/api/upload", {
-            method: "POST",
-            body: formData,
-            headers: {
-              key: userStore.apiToken,
-            },
-          });
-
-          if (response.ok) {
-            toast.success("File uploaded successfully!");
-          } else {
-            toast.error("Error uploading file");
-          }
-        } catch (error) {
-          toast.error(`Error uploading file: ${error}`);
-        }
-      }
-    };
-    fileInput.click();
-  };
-
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
@@ -106,9 +78,8 @@ const Upload: React.FC = () => {
     <div className="flex h-screen bg-[#0d0c0e] text-gray-100">
       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       <main
-        className={`flex-1 overflow-auto p-6 transition-all duration-300 ${
-          sidebarOpen ? "ml-64" : "ml-0"
-        }`}
+        className={`flex-1 overflow-auto p-6 transition-all duration-300 ${sidebarOpen ? "ml-64" : "ml-0"
+          }`}
       >
         <ToastContainer
           position="top-right"
@@ -123,25 +94,30 @@ const Upload: React.FC = () => {
           theme="dark"
         />
         <h1 className="mb-2 text-2xl font-bold">Upload Files</h1>
-        <p className="mb-4 text-gray-400">
-          Use the dropzone above to upload files to your account.
+        <p className="mb-8 text-gray-400">
+          Use the dropzone below to upload files to your account.
         </p>
 
         <div
           ref={dropzoneRef}
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed border-gray-600 rounded-lg cursor-pointer"
-          onDragOver={handleDragOver}
-          onDragLeave={handleDragLeave}
+          className={`flex flex-col items-center justify-center w-full h-[75%] border-2 border-dashed border-zinc-600 rounded-lg cursor-pointer transition-all duration-300"
+            }`}
           onDrop={handleDrop}
           onClick={handleClick}
         >
-          <p className="text-gray-400">
-            Drag and drop files here, or click to select files
+          <Upload className="w-12 h-12 mb-4 text-gray-400" />
+          <p className="text-lg font-medium text-gray-300 mb-2">
+            Drag and drop files here
           </p>
+          <p className="text-sm text-gray-400">or</p>
+          <button className="mt-4 px-4 py-2 bg-purple-500 text-white rounded-md hover:bg-purple-700 transition-colors duration-300 flex items-center">
+            <FileUp className="w-4 h-4 mr-2" />
+            Select Files
+          </button>
         </div>
       </main>
     </div>
   );
 };
 
-export default Upload;
+export default UploadPage;
