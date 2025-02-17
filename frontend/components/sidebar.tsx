78eaaa615b16224333d09a513533e4a19cd7f581
big dick bandit <dylan@tritan.gg>
2025-02-17 02:07:00 -0500

    feat: replace anchor tags with Next.js Link component in sidebar for improved navigation

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,4 +1,5 @@
 import { Image, Settings, User, Menu, Upload } from "lucide-react";
+import Link from "next/link";
 
 const menuItems = [
   { icon: Image, label: "Your Uploads", href: "/dashboard" },
@@ -22,9 +23,8 @@ export function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
         <Menu className="h-6 w-6" />
       </button>
       <aside
-        className={`fixed left-0 top-0 z-10 h-full w-45 bg-[#171619]  transition-transform duration-300 ease-in-out ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
+        className={`fixed left-0 top-0 z-10 h-full w-45 bg-[#171619]  transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
       >
         <div className="flex h-full flex-col">
           <div className="flex items-center justify-between p-4">
@@ -34,14 +34,14 @@ export function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
           </div>
           <nav className="flex-1 space-y-2 p-4">
             {menuItems.map((item) => (
-              <a
+              <Link
                 key={item.label}
                 href={item.href}
                 className="flex items-center rounded-md px-4 py-2 text-gray-300 hover:bg-purple-500 hover:text-white"
               >
                 <item.icon className="mr-3 h-5 w-5" />
                 {item.label}
-              </a>
+              </Link>
             ))}
           </nav>
         </div>
