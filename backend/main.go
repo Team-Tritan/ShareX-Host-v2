50fd479099d6ade15c04e8d5047d59aebf3d9271
big dick bandit <dylan@tritan.gg>
2025-02-17 01:59:05 -0500

    okay!

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
+	"path/filepath"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -28,8 +32,14 @@ func main() {
 	port := config.AppConfigInstance.Port
 	address := fmt.Sprintf(":%d", port)
 
+	templates := loadTemplates()
+
 	app.Use(logger.New())
 	app.Use(cors.New())
+	app.Use(func(c *fiber.Ctx) error {
+		c.Locals("templates", templates)
+		return c.Next()
+	})
 
 	if err := router.SetupRoutes(app); err != nil {
 		sentry.CaptureException(err)
@@ -42,6 +52,32 @@ func main() {
 		sentry.CaptureException(err)
 		log.Fatalf("Error starting server: %v", err)
 	}
+}
+
+func loadTemplates() *template.Template {
+	templates := template.New("")
+	templatePath := "./pages"
+
+	templateDir, err := ioutil.ReadDir(templatePath)
+	if err != nil {
+		sentry.CaptureException(err)
+		log.Fatalf("Error reading template directory: %v", err)
+	}
+
+	for _, fileInfo := range templateDir {
+		templateName := fileInfo.Name()
+		if !fileInfo.IsDir() && strings.HasSuffix(templateName, ".html") {
+			templateBytes, err := ioutil.ReadFile(filepath.Join(templatePath, templateName))
+			if err != nil {
+				sentry.CaptureException(err)
+				log.Fatalf("Error reading template file: %v", err)
+			}
+			templateContent := string(templateBytes)
+			templates.New(templateName).Parse(templateContent)
+		}
+	}
+
+	return templates
 }
 
 func initSentry() {
