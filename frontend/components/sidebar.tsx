1a05a9f374e508f9b288c92489c07aafb30e7690
big dick bandit <dylan@tritan.gg>
2025-02-21 03:27:35 -0500

    feat(frontend/components): Refactor Sidebar and add active link highlighting

--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,5 +1,6 @@
 import { Image, LinkIcon, Menu, Settings, Upload } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const menuItems = [
   { icon: Image, label: "Your Uploads", href: "/dashboard" },
@@ -14,6 +15,7 @@ interface SidebarProps {
 }
 
 export function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
+  const path = usePathname();
   return (
     <>
       <button
@@ -23,27 +25,32 @@ export function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
         <Menu className="h-6 w-6" />
       </button>
       <aside
-        className={`fixed left-0 top-0 z-10 h-full w-45 bg-[#171619]  transition-transform duration-300 ease-in-out ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
+        className={`fixed left-0 top-0 z-10 h-full w-45 bg-gradient-to-b from-[#1c1b1e] to-[#171619] shadow-lg transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
       >
         <div className="flex h-full flex-col">
           <div className="flex items-center justify-between p-4">
-            <h2 className="text-lg font-semibold text-white ml-4 mt-2.5">
+            <h2 className="text-xl font-semibold text-white ml-6 mt-3">
               Tritan Uploader
             </h2>
           </div>
           <nav className="flex-1 space-y-2 p-4">
-            {menuItems.map((item) => (
-              <Link
-                key={item.label}
-                href={item.href}
-                className="flex items-center rounded-md px-4 py-2 text-gray-300 hover:bg-purple-500 hover:text-white"
-              >
-                <item.icon className="mr-3 h-5 w-5" />
-                {item.label}
-              </Link>
-            ))}
+            {menuItems.map((item) => {
+              const active = path === item.href;
+              return (
+                <Link
+                  key={item.label}
+                  href={item.href}
+                  className={`flex items-center rounded-md px-4 py-2 ${active
+                    ? "bg-purple-500 text-white"
+                    : "text-gray-300 hover:bg-purple-500 hover:text-white"
+                    }`}
+                >
+                  <item.icon className="mr-3 h-6 w-6" />
+                  {item.label}
+                </Link>
+              );
+            })}
           </nav>
         </div>
       </aside>
